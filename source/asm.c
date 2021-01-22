58c05e0067f4b4665f3b8a073bf7fbd0411fe683
hms-place <alessandroversari@gmail.com>
2021-01-22 19:21:02 +0100

    erg

--- source/asm.c
+++ source/asm.c
@@ -8,14 +8,14 @@ void Help(){
 	puts(" -o lets you decide the output name \r\n -l gets you an outputname.lis file with infos");
 }
 int main(int argc, char *argv[]) {
-	char * first = argv[1];
+	char * toCompile = argv[1];
 	char toprint [500];
 
 	if (argc < 2){
 		puts(" sup, 'm workin fine \r\n use me to compile .s files  \r\n asm -help if you need help");
 		return 0;
 	}
-	if(strncmp(first,"-help",10) == 0){
+	if(strncmp(toCompile,"-help",10) == 0){
 		Help();
 		return 0;
 	}
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 				return 0;
 			}
 			else{
-				output = malloc(strlen(argv[i]) + 1);
+				output = (char *) malloc(strlen(argv[i]) + 1);
 				strcpy(output,argv[i]);
 			}
 		}
@@ -43,32 +43,42 @@ int main(int argc, char *argv[]) {
 			puts(toprint);
 		}
 	}
+	// if the last thing that you find is a -o there are some probs
 	if(od){
 		puts("you must specify a filename after -o");
 		return 0;
 	}
 	//files check
-	if(!checkExtS(first)) {
+	if(!checkExtS(toCompile)) {
 		puts("error this file is not supported, use *.s");
 		return 1;
 	}
-	char * noext = getName(first);
+	char * noext = getName(toCompile);
 	if(output && Len(getName(output))>8){ //check the output if it was specified
 		puts("the output name is too long");
 		return 1;
 	}
 	//if the output is not specified, output = noext + ".exe"
 	if(!output){
-		output = (char *)malloc(strlen(noext)+ 1 + 4);
+		output = (char *) malloc(strlen(noext) + 1 + 4);
 		sprintf(output,"%s.exe",noext);
 	}
-	//OLD
-	char list[strlen(output) + 10];
+	// add .exe in the output name in case it doesn't have it
+	if(!checkExtExe(output)){
+		int strl = strlen(output);
+		char * tmp = (char *) malloc(strl + 1 + 4);
+		strcpy(tmp, output);
+		strcat(tmp, ".exe");
+		free(output);
+		output = tmp;
+	}
+	char * list = (char *) malloc(strlen(output) + 10);		// e che cazzo me ne fotto
 	if(l)
 		sprintf(list,">>%s.lis",getName(output));
 	else
 		strcpy(list,">NUL");
-	sprintf(toprint,"gcc %s -o %s -Wa,-a -g %s",first,output,list);
+	sprintf(toprint,"gcc %s -o %s -Wa,-a -g %s",toCompile,output,list);
+	puts(toprint);
 	system(toprint);
     return 0; 
 }
--- source/asmlib.h
+++ source/asmlib.h
@@ -46,13 +46,17 @@ int Len(const char* myStr){
 int checkExtExe(const char* myStr){
 	if(!myStr)
 		return 0;
-	int last = myStr[strlen(myStr) - 1]=='e' || myStr[strlen(myStr) - 1]=='E';
-	int lastt = myStr[strlen(myStr) - 2]=='x' || myStr[strlen(myStr) - 2]=='X';
-	int lasttt = myStr[strlen(myStr) - 3]=='e' || myStr[strlen(myStr) - 3]=='E';
-	return last && lastt && lasttt && myStr[strlen(myStr) - 4]=='.';
+	int strl = strlen(myStr);
+	if (strl < 4)
+		return 0;
+	int last = myStr[strl - 1]=='e' || myStr[strl - 1]=='E';
+	int lastt = myStr[strl - 2]=='x' || myStr[strl - 2]=='X';
+	int lasttt = myStr[strl - 3]=='e' || myStr[strl - 3]=='E';
+	return last && lastt && lasttt && myStr[strl - 4]=='.';
 }
 int checkExtS(const char* myStr){
 	if (!myStr) //check for null
 		return 0;
-	return (myStr[strlen(myStr) - 1]=='s' || myStr[strlen(myStr) - 1]=='S') && myStr[strlen(myStr) - 2]=='.';
+	int strl = strlen(myStr);
+	return (myStr[strl - 1]=='s' || myStr[strl - 1]=='S') && myStr[strl - 2]=='.';
 }
